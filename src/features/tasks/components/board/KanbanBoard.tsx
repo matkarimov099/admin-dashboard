00872af4997946ea2248f138a6b8841f47dc393d
Matkarim <matkarimov1099@gmail.com>
2025-12-23 21:02:16 +0500

    data-table column resizing

--- src/features/tasks/components/board/KanbanBoard.tsx
+++
@@ -1,4 +0,0 @@
-export { KanbanBoard } from './KanbanBoard';
-export { KanbanCard } from './KanbanCard';
-export { KanbanColumn } from './KanbanColumn';
-export { KanbanToolbar } from './KanbanToolbar';
